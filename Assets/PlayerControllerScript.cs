a6712aa845d184dada84e42b085529681b71ed6f
sebstar456 <36891233+sebstar456@users.noreply.github.com>
2018-03-08 16:39:24 +0100

    Basic Control Script added
    script + player cube + plane

---
+++ Assets/PlayerControllerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour {
+
+    // Public variables
+    public float walkspeed;
+
+    // Private variables 
+    private Rigidbody rb;
+   private Vector3 moveDirection;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+
+        moveDirection = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
+	}
+
+     void FixedUpdate()
+    {
+        Move();  
+    }
+
+    private void Move()
+    {
+        rb.velocity = moveDirection * walkspeed * Time.deltaTime;
+    }
+
+}
